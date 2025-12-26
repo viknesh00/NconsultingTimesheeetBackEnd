433925af000e9f3409abd978b448fdd24296e280
Nareshkumar <naresh@natobotics.com>
2025-12-26 12:17:29 +0530

    publish settings

--- NconsutingTimesheetApi/Program.cs
+++ NconsutingTimesheetApi/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddCors(options =>
 	options.AddPolicy("AllowFrontend", policy =>
 	{
 		policy
-			.WithOrigins("http://localhost:3000") // <-- Your frontend URL
+			.WithOrigins("https://nconsultingtimesheet.netlify.app", "http://localhost:3000", "https://nconsultingtimesheet.vercel.app")
 			.AllowAnyHeader()
 			.AllowAnyMethod()
 			.AllowCredentials();
